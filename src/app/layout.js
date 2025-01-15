3484409069d02b1401df99020d9ed1505631d703
stevejay675 <stevejay675@gmail.com>
2025-01-15 03:38:59 +0100

    updates

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,6 +25,7 @@ export default function RootLayout({ children }) {
       <body className={`${poppins.variable}`}>
         <Header />
         {children}
+        <BackToTop />
         <Whats />
         <div className="App"><Footer /></div>
       </body>
